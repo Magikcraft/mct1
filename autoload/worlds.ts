9b4190782b8e7e0d8482023a404fab4e436ec9ab
Josh Wulf <josh@magikcraft.io>
2019-07-09 04:20:24 +0300

    tslint ignore

--- autoload/worlds.ts
+++ autoload/worlds.ts
@@ -1,3 +1,4 @@
 // Import for constructor side-effect
 // Cleans up empty worlds, puts others under management
-const WorldManager = require('../world')
+
+const WorldManager = require('../world') // tslint:disable-line
