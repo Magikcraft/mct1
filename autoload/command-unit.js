3b1db222de0e9de4653399a973f1d6f1d02d6952
Josh Wulf <josh@magikcraft.io>
2019-11-02 16:18:47 +1000

    Add units command

---
+++ autoload/command-unit.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user_1 = require("../user");
+var commando_1 = require("../utils/commando");
+commando_1.default('quest', function (args, player) {
+    var mct1Player = user_1.MCT1PlayerCache.getMct1Player(player);
+    var currentUnit = mct1Player.mct1.isUSA ? 'mgdl' : 'mmolL';
+    echo(player, "Currently using " + currentUnit);
+    if (args[0] === 'mmol') {
+        mct1Player.mct1.isUSA = false;
+        echo(player, 'Now using mmolL');
+        return true;
+    }
+    if (args[0] === 'mgdl') {
+        mct1Player.mct1.isUSA = true;
+        echo(player, 'Now using mgdl');
+        return true;
+    }
+    echo(player, 'Usage: /units <mmol | mgdl>');
+    return true;
+});
---
+++ autoload/command-unit.ts
@@ -0,0 +1,20 @@
+import { MCT1PlayerCache } from '../user'
+import commando from '../utils/commando'
+commando('quest', (args, player) => {
+    const mct1Player = MCT1PlayerCache.getMct1Player(player)
+    const currentUnit = mct1Player.mct1.isUSA ? 'mgdl' : 'mmolL'
+    echo(player, `Currently using ${currentUnit}`)
+
+    if (args[0] === 'mmol') {
+        mct1Player.mct1.isUSA = false
+        echo(player, 'Now using mmolL')
+        return true
+    }
+    if (args[0] === 'mgdl') {
+        mct1Player.mct1.isUSA = true
+        echo(player, 'Now using mgdl')
+        return true
+    }
+    echo(player, 'Usage: /units <mmol | mgdl>')
+    return true
+})
